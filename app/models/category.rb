159850a7f0a478fba31df07cb5bafe4c187a6870
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-03-31 20:14:12 -0300

    self join relation Category ready

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
-  belongs_to :category
+  has_many :children_categories, class_name: "Category",
+  foreign_key: "category_id"
+  belongs_to :parent_category, class_name: "Category", optional: true, foreign_key: 'category_id'
+
+  has_and_belongs_to_many :products
 end
